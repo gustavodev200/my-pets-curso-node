aff2db4dd6529b034c1169da60fdeb32265c34c8
gustavodev200 <gustavolage200@gmail.com>
2022-03-21 21:27:01 -0300

    verify token

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,8 +1,10 @@
 const User = require("../models/User");
 
 const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken')
 
 const createUserToken = require("../helpers/create-user-token");
+const getToken = require('../helpers/get-token')
 
 module.exports = class UserController {
   static async register(req, res) {
@@ -98,5 +100,47 @@ module.exports = class UserController {
     }
 
     await createUserToken(user, req, res);
+  }
+
+  static async checkUser(req, res) {
+    let currentUser
+    
+    if(req.headers.authorization){
+
+      const token = getToken(req)
+      const decoded = jwt.verify(token, 'nossosecret')
+
+      currentUser = await User.findById(decoded.id)
+
+      currentUser.password = undefined
+
+    }else{
+      currentUser = null
+    }
+
+    res.status(200).send(currentUser)
+  }
+
+  static async getUserById(req, res) {
+    const id = req.params.id 
+
+    const user = await User.findById(id).select('-password')
+
+    if(!user) {
+      res.status(422).json({
+        message: "Usuário não encontrado",
+      })
+
+      return
+    }
+
+    res.status(200).json({ user })
+  }
+
+  static async editUser(req, res) {
+    res.status(200).json({
+      message: "Deu certo Update",
+    })
+    return
   }
 };
