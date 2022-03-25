1d56d5992f5eb6aeb6ca94703495f2df2f87441e
gustavodev200 <gustavolage200@gmail.com>
2022-03-24 21:50:35 -0300

    atualizacao de usuario e upload de images com multer

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -7,7 +7,7 @@ const jwt = require("jsonwebtoken");
 
 const createUserToken = require("../helpers/create-user-token");
 const getToken = require("../helpers/get-token");
-const getUserByToken = require('../helpers/get-user-by-token')
+const getUserByToken = require("../helpers/get-user-by-token");
 module.exports = class UserController {
   static async register(req, res) {
     const { name, email, phone, password, confirmpassword } = req.body;
@@ -136,52 +136,91 @@ module.exports = class UserController {
   }
 
   static async editUser(req, res) {
-    const id = req.params.id;
+    const token = getToken(req);
 
+    //console.log(token);
 
-    //check if user exists
-    const token = getToken(req)
-    const user = await getUserByToken(token)
+    const user = await getUserByToken(token);
 
+    // console.log(user);
+    // console.log(req.body)
+    // console.log(req.file.filename)
 
-    const {name, email, phone, password, confirmpassword} = req.body
+    const name = req.body.name;
+    const email = req.body.email;
+    const phone = req.body.phone;
+    const password = req.body.password;
+    const confirmpassword = req.body.confirmpassword;
 
-    let image = ''
+    let image = "";
 
-    //validations
+    if (req.file) {
+      image = req.file.filename;
+    }
+
+    // validations
     if (!name) {
-      res.status(422).json({ message: "O nome é obrigatorio" });
-      return;
-    }
-    if (!email) {
-      res.status(422).json({ message: "O e-mail é obrigatorio" });
+      res.status(422).json({ message: "O nome é obrigatório!" });
       return;
     }
 
-    //check if email has already taken
-    const userExists = await User.findOne({email:email});
+    user.name = name;
+
+    if (!email) {
+      res.status(422).json({ message: "O e-mail é obrigatório!" });
+      return;
+    }
+
+    // check if user exists
+    const userExists = await User.findOne({ email: email });
 
     if (user.email !== email && userExists) {
-      res.status(422).json({
-        message: "Por favor, utilize outro e-mail!",
-      });
+      res.status(422).json({ message: "Por favor, utilize outro e-mail!" });
       return;
     }
 
-    user.email = email
+    user.email = email;
+
+    if (image) {
+      const imageName = req.file.filename;
+      user.image = imageName;
+    }
 
     if (!phone) {
-      res.status(422).json({ message: "O telefone é obrigatorio" });
+      res.status(422).json({ message: "O telefone é obrigatório!" });
       return;
     }
-    if (!password) {
-      res.status(422).json({ message: "A senha é obrigatorio" });
-      return;
+
+    user.phone = phone;
+
+    // check if password match
+    if (password != confirmpassword) {
+      res.status(422).json({ error: "As senhas não conferem." });
+
+      // change password
+    } else if (password == confirmpassword && password != null) {
+      // creating password
+      const salt = await bcrypt.genSalt(12);
+      const reqPassword = req.body.password;
+
+      const passwordHash = await bcrypt.hash(reqPassword, salt);
+
+      user.password = passwordHash;
     }
-    if (!confirmpassword) {
-      res.status(422).json({ message: "A confirmação de senha é obrigatorio" });
-      return;
+
+    try {
+      // returns updated data
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: user._id },
+        { $set: user },
+        { new: true }
+      );
+      res.json({
+        message: "Usuário atualizado com sucesso!",
+        data: updatedUser,
+      });
+    } catch (error) {
+      res.status(500).json({ message: error });
     }
-  
   }
 };
--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -4,11 +4,12 @@ const UserController = require('../controllers/UserController')
 
 //middleware
 const verifyToken = require('../helpers/verify-token')
+const {imageUpload} = require("../helpers/image-upload")
 
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
 router.get('/checkuser', UserController.checkUser)
 router.get('/:id', UserController.getUserById)
-router.patch('/edit/:id', verifyToken, UserController.editUser)
+router.patch('/edit/:id', verifyToken, imageUpload.single('image'), UserController.editUser)
 
 module.exports = router
