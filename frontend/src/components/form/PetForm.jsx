a5a96525f9f9363f6cd95d081fb61a58378e805b
gustavodev200 <gustavolage200@gmail.com>
2022-04-07 20:38:54 -0300

    tela adicionar pet quase finalizada

--- frontend/src/components/form/PetForm.jsx
+++ frontend/src/components/form/PetForm.jsx
@@ -1,27 +1,62 @@
-import { useState } from "react";
 
-import formStyles from "./Form.module.css";
-import Input from "./Input";
-import Select from "./Select";
+import { useState } from 'react'
 
-const PetForm = ({ petData, btnText, handleSubmit }) => {
-  const [pet, setPet] = useState(petData || {});
-  const [preview, setPreview] = useState([]);
-  const colors = ["Branco", "Preto", "Cinza", "Caramelo"];
+import formStyles from './Form.module.css'
 
-  const onFileChange = (e) => {};
+import Input from './Input'
+import Select from './Select'
 
-  const handleChange = (e) => {};
+function PetForm({ handleSubmit, petData, btnText }) {
+  const [pet, setPet] = useState(petData || {})
+  const [preview, setPreview] = useState([])
+  const colors = ['Branco', 'Preto', 'Cinza', 'Caramelo']
 
-  const handleColor = (e) => {};
+  function onFileChange(e) {
+    setPreview(Array.from(e.target.files))
+    setPet({ ...pet, images: [...e.target.files] })
+  }
+
+  function handleChange(e) {
+    setPet({ ...pet, [e.target.name]: e.target.value })
+  }
+
+  function handleColor(e) {
+    setPet({
+      ...pet,
+      color: e.target.options[e.target.selectedIndex].text,
+    })
+  }
+
+  const submit = (e) => {
+    e.preventDefault()
+    handleSubmit(pet)
+  }
 
   return (
-    <form className={formStyles.form_container}>
+    <form onSubmit={submit} className={formStyles.form_container}>
+      <div className={formStyles.preview_pet_images}>
+        {preview.length > 0
+          ? preview.map((image, index) => (
+              <img
+                src={URL.createObjectURL(image)}
+                alt={pet.name}
+                key={`${pet.name}+${index}`}
+              />
+            ))
+          : pet.images &&
+            pet.images.map((image, index) => (
+              <img
+                src={`${process.env.REACT_APP_API}/images/pets/${image}`}
+                alt={pet.name}
+                key={`${pet.name}+${index}`}
+              />
+            ))}
+      </div>
       <Input
         text="Imagens do Pet"
         type="file"
         name="images"
-        handleOnchange={onFileChange}
+        handleOnChange={onFileChange}
         multiple={true}
       />
       <Input
@@ -29,36 +64,35 @@ const PetForm = ({ petData, btnText, handleSubmit }) => {
         type="text"
         name="name"
         placeholder="Digite o nome"
-        handleOnchange={handleChange}
-        value={pet.name || ""}
+        handleOnChange={handleChange}
+        value={pet.name || ''}
       />
       <Input
         text="Idade do Pet"
-        type="text"
+        type="number"
         name="age"
         placeholder="Digite a idade"
-        handleOnchange={handleChange}
-        value={pet.age || ""}
+        handleOnChange={handleChange}
+        value={pet.age || ''}
       />
-
       <Input
         text="Peso do Pet"
         type="number"
         name="weight"
-        placeholder="Digite o peso"
-        handleOnchange={handleChange}
-        value={pet.weight || ""}
+        placeholder="Digite o peso aproximado"
+        value={pet.weight || ''}
+        handleOnChange={handleChange}
       />
       <Select
         name="color"
-        text="Selecione a cor"
+        text="Selecione a categoria"
         options={colors}
         handleOnChange={handleColor}
-        value={pet.color || ""}
+        value={pet.color || ''}
       />
       <input type="submit" value={btnText} />
     </form>
-  );
-};
+  )
+}
 
-export default PetForm;
+export default PetForm
--- frontend/src/components/pages/Pet/AddPet.jsx
+++ frontend/src/components/pages/Pet/AddPet.jsx
@@ -11,13 +11,23 @@ import { useNavigate } from "react-router-dom";
 import useFlashMessage from '../../../hooks/useFlashMessage';
 
 const AddPet = () => {
+    const [token] = useState(localStorage.getItem('token') || '')
+    const {setFlashMessage} = useFlashMessage()
+    const navigate = useNavigate();
+
+    const registerPet = async (pet) => {
+        let msgType = 'success'
+
+        const formData = new FormData
+    }
+
     return ( 
         <section className={styles.addpet_header}>
             <div>
                 <h1>Cadastre um Pet</h1>
                 <p>Depois ele ficará disponível para adoção</p>
             </div>
-            <PetForm btnText="Cadastrar"/>
+            <PetForm handleSubmit={registerPet} btnText="Cadastrar"/>
         </section>
      );
 }
