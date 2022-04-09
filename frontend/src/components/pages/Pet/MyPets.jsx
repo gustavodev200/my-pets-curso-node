98a0dc2d6691fcf010ff7613024426896bc5d398
gustavodev200 <gustavolage200@gmail.com>
2022-04-09 11:29:23 -0300

    minha dashboards de Pets

--- frontend/src/components/pages/Pet/MyPets.jsx
+++ frontend/src/components/pages/Pet/MyPets.jsx
@@ -5,6 +5,8 @@ import api from "../../../utils/api";
 
 import RoundedImage from "../../layout/RoundedImage";
 
+import styles from './Dashboard.module.css'
+
 //hooks
 import useFlashMessage from "../../../hooks/useFlashMessage";
 
@@ -25,21 +27,54 @@ const MyPets = () => {
       });
   }, [token]);
 
+  async function removePet(id) {
+    let msgType = 'success'
+
+    const data = await api.delete(`/pets/${id}`, {
+      headers: {
+        Authorization: `Bearer ${JSON.parse(token)}`,
+      }
+    }).then(response => {
+      const updatedPets = pets.filter(pet => pet._id !== id)
+      setPets(updatedPets)
+      return response.data
+    }).catch(error => {
+      msgType = 'error'
+      return error.response.data
+    })
+
+    setFlashMessage(data.message, msgType)
+  }
+
   return (
     <section>
-      <div>
+      <div className={styles.petslist_header}>
         <h1>Meus Pets</h1>
         <Link to="/pet/add">Cadastrar Pet</Link>
       </div>
-      <div>
+      <div className={styles.petslist_container}>
         {pets.length > 0 &&
           pets.map((pet) => (
-            <div key={pet.id}>
+            <div key={pet.id} className={styles.petlist_row}>
               <RoundedImage
-                src={URL.createObjectURL(image)}
+                src={`${process.env.REACT_APP_API}/images/pets/${pet.images[0]}`}
                 alt={pet.name}
-                width="75px"
+                width="px75"
               />
+              <span className="bold">{pet.name}</span>
+              <div className={styles.actions}>
+                {pet.available ? (
+                  <>
+                    {pet.adopter && <button className={styles.conclude_btn}>Concluir adoção</button>}
+                    <Link to={`/pet/edit/${pet.id}`}>Editar</Link>
+                    <button onClick={() => {
+                      removePet(pet._id)
+                    }}>Excluir</button>
+                  </>
+                ) : (
+                  <p>Pet já adotado</p>
+                )}
+              </div>
             </div>
           ))}
         {pets.length === 0 && <p>Não há Pets cadastrados</p>}
