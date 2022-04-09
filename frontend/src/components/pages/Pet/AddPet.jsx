3695389762c61958b190fbc8e7b87b445f43b62d
gustavodev200 <gustavolage200@gmail.com>
2022-04-08 22:06:35 -0300

    add pet

--- frontend/src/components/pages/Pet/AddPet.jsx
+++ frontend/src/components/pages/Pet/AddPet.jsx
@@ -1,35 +1,63 @@
-import api from '../../../utils/api';
+import api from "../../../utils/api";
 
-import styles from './AddPet.module.css'
+import styles from "./AddPet.module.css";
 
-import PetForm from '../../form/PetForm';
+import PetForm from "../../form/PetForm";
 
-
-import { useState } from 'react'; 
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import useFlashMessage from '../../../hooks/useFlashMessage';
+import useFlashMessage from "../../../hooks/useFlashMessage";
 
 const AddPet = () => {
-    const [token] = useState(localStorage.getItem('token') || '')
-    const {setFlashMessage} = useFlashMessage()
-    const navigate = useNavigate();
+  const [token] = useState(localStorage.getItem("token") || "");
+  const { setFlashMessage } = useFlashMessage();
+  const navigate = useNavigate();
 
-    const registerPet = async (pet) => {
-        let msgType = 'success'
+  const registerPet = async (pet) => {
+    let msgType = "success";
 
-        const formData = new FormData
+    const formData = new FormData();
+
+    await Object.keys(pet).forEach((key) => {
+      if (key === "images") {
+        for (let i = 0; i < pet[key].length; i++) {
+          formData.append("images", pet[key][i]);
+        }
+      } else {
+        formData.append(key, pet[key]);
+      }
+    });
+
+    const data = await api
+      .post("pets/create", formData, {
+        Authorization: `Bearer ${JSON.parse(token)}`,
+        "Content-Type": "multipart/form-data",
+      })
+      .then((res) => {
+        return res.data;
+      })
+      .catch((error) => {
+        msgType = "error";
+        return error.response.data;
+      });
+
+    setFlashMessage(data.message, msgType);
+
+    if (msgType !== "error") {
+      navigate("/pets/mypets")
     }
+  };
 
-    return ( 
-        <section className={styles.addpet_header}>
-            <div>
-                <h1>Cadastre um Pet</h1>
-                <p>Depois ele ficará disponível para adoção</p>
-            </div>
-            <PetForm handleSubmit={registerPet} btnText="Cadastrar"/>
-        </section>
-     );
-}
- 
+  return (
+    <section className={styles.addpet_header}>
+      <div>
+        <h1>Cadastre um Pet</h1>
+        <p>Depois ele ficará disponível para adoção</p>
+      </div>
+      <PetForm handleSubmit={registerPet} btnText="Cadastrar" />
+    </section>
+  );
+};
+
 export default AddPet;
--- frontend/src/components/pages/Pet/MyPets.jsx
+++ frontend/src/components/pages/Pet/MyPets.jsx
@@ -1,8 +1,29 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
+import api from "../../../utils/api";
+
+import RoundedImage from "../../layout/RoundedImage";
+
+//hooks
+import useFlashMessage from "../../../hooks/useFlashMessage";
+
 const MyPets = () => {
   const [pets, setPets] = useState([]);
+  const [token] = useState(localStorage.getItem("token") || "");
+  const { setFlashMessage } = useFlashMessage();
+
+  useEffect(() => {
+    api
+      .get("/pets/mypets", {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(token)}`,
+        },
+      })
+      .then((res) => {
+        setPets(res.data.pets);
+      });
+  }, [token]);
 
   return (
     <section>
@@ -11,7 +32,16 @@ const MyPets = () => {
         <Link to="/pet/add">Cadastrar Pet</Link>
       </div>
       <div>
-        {pets.length > 0 && <p>Meus Pets cadastrados</p>}
+        {pets.length > 0 &&
+          pets.map((pet) => (
+            <div key={pet.id}>
+              <RoundedImage
+                src={URL.createObjectURL(image)}
+                alt={pet.name}
+                width="75px"
+              />
+            </div>
+          ))}
         {pets.length === 0 && <p>Não há Pets cadastrados</p>}
       </div>
     </section>
